b56ea2d1d5ac8edd58253627f29ec9a715adbd5e
João Marcos <josehps2008@gmail.com>
2026-04-10 08:47:38 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,40 +1,35 @@
 window.onload = function(){
 
-let savedTheme = localStorage.getItem("theme") || "dark";
+let savedTheme = localStorage.getItem("theme") || "dark"
 
-document.documentElement.classList.remove("dark","light");
+document.documentElement.classList.remove("dark","light")
 
-document.documentElement.classList.add(savedTheme);
+document.documentElement.classList.add(savedTheme)
 
-let btn = document.getElementById("themeBtn");
+let btn = document.getElementById("themeBtn")
 
-if(btn){
-btn.innerText = savedTheme === "dark" ? "🌙" : "☀️";
-}
+btn.innerText = savedTheme === "dark" ? "🌙" : "☀️"
 
 }
 
 function toggleTheme(){
 
-let html = document.documentElement;
-
-let btn = document.getElementById("themeBtn");
+let html = document.documentElement
+let btn = document.getElementById("themeBtn")
 
 if(html.classList.contains("dark")){
 
-html.classList.replace("dark","light");
-
-localStorage.setItem("theme","light");
-
-btn.innerText="☀️";
+html.classList.remove("dark")
+html.classList.add("light")
+localStorage.setItem("theme","light")
+btn.innerText="☀️"
 
 }else{
 
-html.classList.replace("light","dark");
-
-localStorage.setItem("theme","dark");
-
-btn.innerText="🌙";
+html.classList.remove("light")
+html.classList.add("dark")
+localStorage.setItem("theme","dark")
+btn.innerText="🌙"
 
 }
 
